a26d8c60ee364e4a8a7a9c360f5ca1ad8663c9c8
Infinity101-jpg <68457668+Infinity101-jpg@users.noreply.github.com>
2020-12-23 10:26:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ with open('main.dark', 'r') as file:
     file_contents = file.read()
 stra = '''
 print(" " * 15000)
-
 def typeof(z):
     if type(z) == int:
         return 'Intiger';
-
     if type(z) == float:
         return 'Float';
-
     if type(z) == str:
         return 'String';
-
     if type(z) == bool:
         return 'Boolean';
 true = True
@@ -20,16 +16,13 @@ false = False
 import random as r
 def random(x, y):
   return r.randint(x, y)
-
-
 def write(a):
     print("  --> " + str(a))
 def read(b):
     input(b)
-    
+read = input
 def lengthof(x):
   return(len(x))
-
 def run():
     main()
     print("  --> Process Has Completed.")
